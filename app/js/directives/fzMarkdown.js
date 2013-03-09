fac34815cf00ccf97468ad2d793215060da7b353
jhosm <joao.hugo.miranda@gmail.com>
2013-03-09 00:13:09 +0000

    The documentation now supports markdown

---
+++ app/js/directives/fzMarkdown.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Courtesy from: http://jsfiddle.net/8bENp/139/
+angular.module('formz.directives').
+directive('fzMarkdown', function MarkdownFactory() {
+	var converter = new Showdown.converter();
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+
+			attrs.$observe('documentation', function(value) {
+				var htmlText = converter.makeHtml(value);
+				element.html(htmlText);
+			});
+		}
+	}
+});
